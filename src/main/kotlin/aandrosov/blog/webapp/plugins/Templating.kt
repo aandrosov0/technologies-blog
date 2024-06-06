481e85491c06deb70cb54d48463903615eeb506f
aandrosov <aandrosov338@gmail.com>
2024-06-06 13:19:04 +0300

    Set Default Encoding For Templating Plugin

--- src/main/kotlin/aandrosov/blog/webapp/plugins/Templating.kt
+++ src/main/kotlin/aandrosov/blog/webapp/plugins/Templating.kt
@@ -6,6 +6,7 @@ import io.ktor.server.freemarker.*
 
 fun Application.configureTemplating() {
     install(FreeMarker) {
+        defaultEncoding = "UTF-8"
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
     }
 }
